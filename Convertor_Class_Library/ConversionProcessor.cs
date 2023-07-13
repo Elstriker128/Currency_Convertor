377a5ce0e7d74e86518e47797240cf66edb638d5
David Stan <89691298+Elstriker128@users.noreply.github.com>
2023-07-13 23:30:22 +0300

    Adding the class library

---
+++ Convertor_Class_Library/ConversionProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convertor_Class_Library
+{
+    public class ConversionProcessor
+    {
+        public static async Task<ConversionModel> LoadConversion(string baseCode, string targetCode, decimal inputAmount)
+        {
+            //string url = $"https://v6.exchangerate-api.com/v6/{apiKey}/pair/{baseCode}/{targetCode}/{inputAmount}";
+            using(HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync($"pair/{baseCode}/{targetCode}/{inputAmount}/"))
+            {
+                if(response.IsSuccessStatusCode)
+                {
+                    ConversionModel conversionModel = await response.Content.ReadAsAsync<ConversionModel>();
+
+                    return conversionModel;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
